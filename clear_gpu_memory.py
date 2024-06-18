9314923f23e8eb514d4e54e3ff878500aec7c5d0
Michael Lo <lomic8@gmail.com>
2024-06-18 10:19:58 -0700

    Added script for monitoring and clearing memory

---
+++ clear_gpu_memory.py
@@ -0,0 +1,14 @@
+import torch
+
+def clear_gpu_memory():
+	# Clear PyTorch cache
+	torch.cuda.empty_cache()
+
+	# Print GPU memory usage
+	for i in range(torch.cuda.device_count()):
+		print(f"Memory Usage of GPU {i}:")
+		print(f"  Allocated: {torch.cuda.memory_allocated(i) / 1024**3:.2f} GB")
+		print(f"  Cached:    {torch.cuda.memory_reserved(i_ / 1024**3:.2f} GB")
+
+if __name__ == "__main__":
+	clear_gpu_memory()
