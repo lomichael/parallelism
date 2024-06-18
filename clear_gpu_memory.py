31cb798b88f92d9402411e0ff53b36c3cbc8c61f
Michael Lo <lomic8@gmail.com>
2024-06-18 10:20:46 -0700

    Fixed typo in clear_gpu_memory.py

--- clear_gpu_memory.py
+++ clear_gpu_memory.py
@@ -8,7 +8,7 @@ def clear_gpu_memory():
 	for i in range(torch.cuda.device_count()):
 		print(f"Memory Usage of GPU {i}:")
 		print(f"  Allocated: {torch.cuda.memory_allocated(i) / 1024**3:.2f} GB")
-		print(f"  Cached:    {torch.cuda.memory_reserved(i_ / 1024**3:.2f} GB")
+		print(f"  Cached:    {torch.cuda.memory_reserved(i) / 1024**3:.2f} GB")
 
 if __name__ == "__main__":
 	clear_gpu_memory()
